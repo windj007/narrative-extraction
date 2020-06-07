57937426f06746c9f8a77b1234cc4d5bd6cf5d02
Roman Suvorov <windj007@gmail.com>
2020-06-07 12:51:20 +0300

    dockr run

---
+++ bin/rundocker.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+SRCDIR="$(dirname $0)/.."
+SRCDIR="$(realpath $SRCDIR)"
+DATADIR="$(dirname $0)/../../data"
+DATADIR="$(realpath $DATADIR)"
+
+
+docker run -ti \
+    --name "narrative-extraction-$RANDOM" \
+    --rm \
+    --gpus=all \
+    --ipc=host \
+    -v "$SRCDIR":/workspace \
+    -v "$DATADIR":/data \
+    -e TORCH_HOME=/data/pretrained_models \
+    -e CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES \
+    rsuvorov/narrative-extraction \
+    $@
