e3c3cfb711708c509c6001c98b1134de5dcaa472
Roman Suvorov <windj007@gmail.com>
2020-07-09 22:33:09 +0300

    debug

--- narratex/segmentation.py
+++ narratex/segmentation.py
@@ -15,7 +15,7 @@ def infer_segmentation(all_texts, window_size=5, num_topics=20, passes=5):
     vocab = gensim.corpora.dictionary.Dictionary(chunks)
     chunk_bow = [vocab.doc2bow(ch) for ch in chunks]
     print(chunk_bow[:2])
-    topic_model = gensim.models.LdaModel(chunk_bow, num_topics=num_topics, id2word=vocab.id2token, passes=passes)
+    topic_model = gensim.models.LdaModel(chunk_bow, num_topics=num_topics, passes=passes)
     vectors = [topic_model[ch] for ch in chunk_bow]
     print(vectors[:1])
 
