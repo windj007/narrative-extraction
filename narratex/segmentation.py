6d941b8f9f92faf1a8f69455f2609cae58e23ecf
Roman Suvorov <windj007@gmail.com>
2020-07-09 23:14:34 +0300

    debug

--- narratex/segmentation.py
+++ narratex/segmentation.py
@@ -1,10 +1,15 @@
 import gensim
+import numpy as np
+
+
+def calc_topic_entropy(topic_proba_pairs):
+    return -sum(p * np.log2(p) for _, p in topic_proba_pairs)
 
 
 GOOD_POS = {'VERB', 'ADJ', 'ADV', 'INTJ', 'NOUN', 'PROPN'}
 
 
-def infer_segmentation(all_texts, window_size=5, num_topics=20, passes=5):
+def infer_segmentation(all_texts, window_size=5, num_topics=40, passes=10):
     all_texts_with_tokens = [[[tok.lemma.lower() for tok in sent.joint
                                if tok.upos in GOOD_POS]
                               for sent in doc]
@@ -19,12 +24,13 @@ def infer_segmentation(all_texts, window_size=5, num_topics=20, passes=5):
 
     vocab = gensim.corpora.dictionary.Dictionary(chunks)
     vocab[0]
-    print(vocab.id2token)
     chunk_bow = [vocab.doc2bow(ch) for ch in chunks]
-    print(chunk_bow[:2])
     topic_model = gensim.models.LdaMulticore(chunk_bow, num_topics=num_topics, passes=passes, id2word=vocab.id2token)
-    vectors = [topic_model[ch] for ch in chunk_bow]
-    print(vectors[:1])
+    chunk_topics = [topic_model[ch] for ch in chunk_bow]
+    chunk_entropy = [calc_topic_entropy(tops) for tops in chunk_topics]
+    print('min', min(chunk_entropy), 'max', max(chunk_entropy), np.mean(chunk_entropy))
+    print(chunk_entropy)
+
 
 
 
