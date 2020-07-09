4fbdbb6e155c939681dff70dafdcfdd7cedb6111
Roman Suvorov <windj007@gmail.com>
2020-07-09 22:43:34 +0300

    debug

--- narratex/segmentation.py
+++ narratex/segmentation.py
@@ -1,7 +1,13 @@
 import gensim
 
+
+GOOD_POS = {'VERB', 'ADJ', 'ADV', 'INTJ', 'NOUN', 'PROPN'}
+
+
 def infer_segmentation(all_texts, window_size=5, num_topics=20, passes=5):
-    all_texts_with_tokens = [[[tok.lemma.lower() for tok in sent.joint] for sent in doc]
+    all_texts_with_tokens = [[[tok.lemma.lower() for tok in sent.joint
+                               if tok.upos in GOOD_POS]
+                              for sent in doc]
                              for doc in all_texts]
     chunks = []
     for doc in all_texts_with_tokens:
