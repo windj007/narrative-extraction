8306d302ee5362b815850ba28f8fed46077bc2c5
Roman Suvorov <windj007@gmail.com>
2020-07-09 22:39:44 +0300

    debug

--- narratex/segmentation.py
+++ narratex/segmentation.py
@@ -1,6 +1,5 @@
 import gensim
 
-
 def infer_segmentation(all_texts, window_size=5, num_topics=20, passes=5):
     all_texts_with_tokens = [[[tok.lemma.lower() for tok in sent.joint] for sent in doc]
                              for doc in all_texts]
@@ -13,9 +12,11 @@ def infer_segmentation(all_texts, window_size=5, num_topics=20, passes=5):
             chunks.append(cur_chunk)
 
     vocab = gensim.corpora.dictionary.Dictionary(chunks)
+    vocab[0]
+    print(vocab.id2token)
     chunk_bow = [vocab.doc2bow(ch) for ch in chunks]
     print(chunk_bow[:2])
-    topic_model = gensim.models.LdaModel(chunk_bow, num_topics=num_topics, passes=passes)
+    topic_model = gensim.models.LdaMulticore(chunk_bow, num_topics=num_topics, passes=passes, id2word=vocab.id2token)
     vectors = [topic_model[ch] for ch in chunk_bow]
     print(vectors[:1])
 
